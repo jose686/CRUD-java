7904b26c0986b3085a3f74b6319605025ff23ce7
jose686 <jose686691@gmail.com>
2024-06-07 21:05:00 +0200

    creando agregar crud

---
+++ src/Modelo/Conexion.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author pepe6
+ */
+public class Conexion {
+    
+}
